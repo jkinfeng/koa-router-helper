c22b7f90e58286062da4b8a9fd8f0a763425ae42
Jkin.feng <312943362@qq.com>
2020-06-12 15:49:43 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# koa-router-helper
-autoload router for koa-router
+# Autoload koa-router
+Autoload router for koa-router
+
+## Installation
+```
+npm i koa-router-helper
+```
+
+## Usage
+```
+const koa = require('koa');
+const autoloadRouter = require('koa-router-helper');
+const app = new koa();
+// ------------------------------------------------
+app.use(autoloadRouter());
+// -----------------or-----------------------------
+app.use(autoloadRouter({
+   path: <router path>,
+   regular: <router file>
+}));
+// ------------------------------------------------
+```
+
+### Options
+```
+path: router files path
+  default: process.cwd()
+  example: require('path').join(process.cwd(), 'routers')
+  
+regular: regular expression matching files
+  default: /router.js$/
+  example: a.router.js, bbb.router.js ...
+```
+
+## Version
+1.0.1
+
+## Copyright
+(C) 2020 Jkin.Feng
+
+## Licensed
+MIT
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const compose = require('koa-compose');
+
+module.exports = autoLoadRouter;
+
+function autoLoadRouter($options) {
+    const $opts = $options || Object.assign({
+        path: process.cwd(),
+        regular: /router.js$/
+    }, $options);
+
+    let __arr = [];
+    searchFiles($opts).forEach(function ($routeFile) {
+        __arr.push(require($routeFile).routes());
+    });
+
+    return compose(__arr);
+}
+
+function searchFiles($opts) {
+    return {
+        files: [],
+        search: function ($path) {
+            let __self = this;
+            if (fs.existsSync($path)) {
+                fs.readdirSync($path).forEach(function ($fileOrDirectory) {
+                    let __selected = path.join($path, $fileOrDirectory);
+                    if (fs.statSync(__selected).isFile()) {
+                        if ($opts.regular.test(__selected.split(path.sep).pop())) {
+                            __self.files.push(__selected);
+                        }
+                    } else {
+                        __self.search(__selected);
+                    }
+                });
+            }
+        },
+        exec: function ($opts) {
+            this.search($opts.path);
+            return this.files;
+        }
+    }.exec($opts);
+}
